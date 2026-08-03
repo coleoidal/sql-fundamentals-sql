6bc34f8d33803a262d7da1e2ea29f3d2e8ff12ac
Kyer Potts <kyerpotts@squidlabs.dev>
2026-08-04 00:00:49 +0800

    done 08
    done 08

--- main.sql
+++ main.sql
@@ -1,15 +1,17 @@
-CREATE TABLE authors (id INTEGER, name TEXT);
-CREATE TABLE books (title TEXT, author_id INTEGER);
+CREATE TABLE employees (name TEXT, salary INTEGER);
+INSERT INTO employees VALUES
+    ('Ada', 95000),
+    ('Bob', 50000),
+    ('Carol', 80000),
+    ('Dan', 55000);
 
-INSERT INTO authors VALUES (1, 'Asimov'), (2, 'Le Guin'), (3, 'Tolkien');
-INSERT INTO books VALUES
-    ('Foundation', 1),
-    ('Lord of the Rings', 3),
-    ('Hobbit', 3),
-    ('Dispossessed', 2);
+UPDATE employees
+  SET salary = 90000
+  WHERE name = 'Bob';
 
-SELECT b.title, a.name
-FROM books b
-INNER JOIN authors a
-ON b.author_id = a.id
-ORDER BY b.title;
+DELETE FROM employees
+WHERE name = 'Carol';
+
+SELECT name, salary
+FROM employees
+ORDER By name;
