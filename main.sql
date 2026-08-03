48518ff36e06382a1a6ecf75f2b60f93e4673eeb
Kyer Potts <kyerpotts@squidlabs.dev>
2026-08-03 23:21:34 +0800

    done 03
    done 03
    
    done 03
    
    done 03

--- main.sql
+++ main.sql
@@ -1,1 +1,8 @@
-SELECT 'Hello, ' || 'SQL!';
+
+CREATE TABLE cities (name TEXT, country TEXT);
+INSERT INTO cities VALUES ('Tokyo', 'Japan');
+INSERT INTO cities VALUES ('Paris', 'France');
+INSERT INTO cities VALUES ('Cairo', 'Egypt');
+
+SELECT name
+FROM cities;
