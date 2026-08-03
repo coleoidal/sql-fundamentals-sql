4d4f5e00512a688b5521249302b871ae21a37761
Kyer Potts <kyerpotts@squidlabs.dev>
2026-08-03 23:53:14 +0800

    done 07
    done 07
    
    done 07

--- main.sql
+++ main.sql
@@ -1,10 +1,15 @@
-CREATE TABLE products (category TEXT, price INTEGER);
-INSERT INTO products VALUES ('food', 10);
-INSERT INTO products VALUES ('tech', 500);
-INSERT INTO products VALUES ('food', 25);
-INSERT INTO products VALUES ('tech', 1500);
-INSERT INTO products VALUES ('book', 30);
+CREATE TABLE authors (id INTEGER, name TEXT);
+CREATE TABLE books (title TEXT, author_id INTEGER);
 
-SELECT category, SUM(price) FROM products
-GROUP BY category
-ORDER BY category;
+INSERT INTO authors VALUES (1, 'Asimov'), (2, 'Le Guin'), (3, 'Tolkien');
+INSERT INTO books VALUES
+    ('Foundation', 1),
+    ('Lord of the Rings', 3),
+    ('Hobbit', 3),
+    ('Dispossessed', 2);
+
+SELECT b.title, a.name
+FROM books b
+INNER JOIN authors a
+ON b.author_id = a.id
+ORDER BY b.title;
